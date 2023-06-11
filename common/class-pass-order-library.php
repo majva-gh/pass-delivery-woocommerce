961bd414d085bb6302696b29983423fe9bbd60ac
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-06-11 17:10:04 +0330

    feet (common/class-pass-order-library@__construct): add public $curlOptions and constructor method

--- common/class-pass-order-library.php
+++ common/class-pass-order-library.php
@@ -3,5 +3,23 @@
 if (!class_exists('Pass_Order_Library')) {
     class Pass_Order_Library
     {
+        private array $curlOptions;
+
+        public function __construct($token)
+        {
+            $this->curlOptions = [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_FAILONERROR => true,
+                CURLOPT_HTTPHEADER => [
+                    "Authorization: Bearer ". $token,//$token is your token created in dashboard.
+                    "Content-Type: application/json; charset=utf-8",
+                    "Accept: application/json"
+                ]
+            ];
+        }
     }
 }
