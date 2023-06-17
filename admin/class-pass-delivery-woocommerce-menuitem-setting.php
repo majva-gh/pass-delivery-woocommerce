18a3540b71c1c2dfe5ae96abf0bdda8babdd0fee
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-06-17 17:55:27 +0330

    feet (admin/class-pass-delivery-woocommerce-menuitem-setting@get_wc_settings_url): remove method

--- admin/class-pass-delivery-woocommerce-menuitem-setting.php
+++ admin/class-pass-delivery-woocommerce-menuitem-setting.php
@@ -23,16 +23,5 @@ if (!class_exists('Pass_Delivery_Woocommerce_Menuitem_Setting')) {
             });
 
         }
-
-        /**
-         * @return boolean
-         * @since  1.0.0
-         */
-        private function get_wc_settings_url(): bool|string
-        {
-
-            return version_compare(WC()->version, '2.1', '>=') ? 'wc-settings' : 'woocommerce_settings';
-
-        }
     }
 }
