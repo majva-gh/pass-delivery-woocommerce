058202a6a19fda46af7eaf53c0e46ed80aed03dd
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-06-18 13:58:52 +0330

    feet (common/class-pass-delivery-helper@get_support_url): movie method

--- common/class-pass-delivery-helper.php
+++ common/class-pass-delivery-helper.php
@@ -3,6 +3,10 @@
 if (!class_exists('Blue_Pass_Delivery_Helper')) {
     class Blue_Pass_Delivery_Helper
     {
+        public function get_support_url()
+        {
+            return admin_url( 'admin.php?page=pass-support' );
+        }
     }
 
     $pdHelper = new Blue_Pass_Delivery_Helper();
