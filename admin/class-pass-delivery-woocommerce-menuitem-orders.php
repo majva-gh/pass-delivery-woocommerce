023aaa67b18158ad821aa914bdd8844d1a611a8c
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-06-19 18:10:25 +0330

    feet (admin/class-pass-delivery-woocommerce-menuitem-orders@add_support_to_admin_menu_item): get order list

--- admin/class-pass-delivery-woocommerce-menuitem-orders.php
+++ admin/class-pass-delivery-woocommerce-menuitem-orders.php
@@ -15,7 +15,18 @@ if (!class_exists('Pass_Delivery_Woocommerce_Menuitem_Orders')) {
         {
             add_submenu_page(Pass_Delivery_Woocommerce_Admin_Panel::$parent_slug, __('Orders', PASS_TRANSLATE_ID), __('Orders', PASS_TRANSLATE_ID), 'manage_options', 'pass-orders', function () {
                 GLOBAL $pdHelper;
-                echo $pdHelper->template_or_error_for_not_found_user($this->load_support_template());
+                if(!$pdHelper->validate_user()) {
+                    echo $pdHelper->error_for_not_found_user();
+                    return;
+                }
+
+
+                $api_key = $pdHelper->get_setting_key('api_key');
+                require_once(PASS_PLUGIN_DIR . '/common/class-pass-order-library.php');
+                $order = new Pass_Order_Library($api_key);
+                $response = $order->list();
+
+                echo $this->load_support_template($response);
             });
         }
 
@@ -38,7 +49,21 @@ if (!class_exists('Pass_Delivery_Woocommerce_Menuitem_Orders')) {
                             <div class="pass-shipping-wc-card">
                                 <h3>Hello, this is <a href="https://pass.qa" target="_blank">Pass</a> support</h3>
                             </div>
-                        </div>';
+                        </div>
+                    </div>
+                
+                
+                    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+                    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+                
+                    <script>
+                        $(document).ready( function () {
+                            $("#myTable").DataTable();
+                            $("#myTable_filter").addClass("d-none");
+                        } );
+                    </script>';
+            return $msg;
         }
     }
 }
