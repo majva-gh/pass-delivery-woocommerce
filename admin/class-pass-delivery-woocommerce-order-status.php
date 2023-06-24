2acab2e9285c49cf9d37356ff74bfe2d023434a7
Majva <m.vahidkhoo@gmail.com>
2023-06-24 15:27:30 +0330

    feat(Pass_Delivery_Woocommerce_Order_Status) : create filter_wc_register_post_statuses method

--- admin/class-pass-delivery-woocommerce-order-status.php
+++ admin/class-pass-delivery-woocommerce-order-status.php
@@ -6,5 +6,18 @@ if (!class_exists('Pass_Delivery_Woocommerce_Order_Status')) {
         public function __construct()
         {
         }
+
+        // Register New Order Statuses
+        function filter_wc_register_post_statuses()
+        {
+            register_post_status('wc-shipped-with-pass', array(
+                'label'                     => _x('Shipped With Pass', 'Order status', 'default'),
+                'public'                    => true,
+                'exclude_from_search'       => false,
+                'show_in_admin_all_list'    => true,
+                'show_in_admin_status_list' => true,
+                'label_count'               => _n_noop('Shipped With Pass (%s)', 'Shipped With Pass (%s)')
+            ));
+        }
     }
 }
