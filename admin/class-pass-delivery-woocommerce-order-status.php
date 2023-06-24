eeef98a1379f2d1b40e4dd5c6ebd7616b1a58eff
Majva <m.vahidkhoo@gmail.com>
2023-06-24 15:25:54 +0330

    feat(admin): create Pass_Delivery_Woocommerce_Order_Status class

---
+++ admin/class-pass-delivery-woocommerce-order-status.php
@@ -0,0 +1,10 @@
+<?php
+if (!class_exists('Pass_Delivery_Woocommerce_Order_Status')) {
+    class Pass_Delivery_Woocommerce_Order_Status
+    {
+
+        public function __construct()
+        {
+        }
+    }
+}
